be18564609d17e1715d934911e719f78c1fde51b
Nikin Baidar <nikinbaidarr@gmail.com>
2023-04-06 22:09:44 +0545

    playing with react routers

--- src/App.js
+++ src/App.js
@@ -1,1 +1,48 @@
+import React from 'react';
 
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+function Main() {
+    return(
+        <div>
+            <ul>
+                <li><a href={`/home`}>Home</a></li>
+                <li><a href={`/about`}>About</a></li>
+                <li><a href={`/support`}>Support</a></li>
+            </ul>
+        </div>
+    );
+}
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main/>
+    },
+    {
+        path: "/home",
+        element: <h1>Home</h1>
+    },
+    {
+        path: "/about",
+        element: <h1>About</h1>
+    },
+    {
+        path: "/support",
+        element: <h1>Support</h1>
+    },
+])
+
+
+
+const App = () => {
+  return (
+      <RouterProvider router={router} />
+  );
+};
+
+export default App;
