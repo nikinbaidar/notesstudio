1494cc957c179ed2e6759f0eec29e0ba972fcab7
Nikin Baidar <nikinbaidarr@gmail.com>
2023-04-22 20:29:08 +0545

    remember selected radio with local storage persistance

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,6 +3,171 @@ import { SEO } from './seo';
 
 import { loadCurriculum, loadQuiz } from '../dataLoader'
 
+export class Quiz extends React.Component {
+
+    constructor(props) {
+        super(props);
+        const totalQuestions = 10;
+        const selectedOptions = Array.from({totalQuestions}, (_, i) => 
+            `question${i+1}`).reduce((acc, key) => ({ ...acc, [key]: null }), {});
+        this.correctValues = [];
+        this.quizData = loadQuiz();
+        this.numberOfQuestionsPerPage = 5;
+        this.state = {
+            currentPage: -1,
+            start: 0,
+            end: 0,
+            selectedOptions: selectedOptions,
+            answered: 0,
+        }
+    }
+
+    componentDidMount() {
+        this.quizData.forEach((item) => {
+            const correctAnswer = item.options[item.correctAnswerIndex];
+            this.correctValues.push(correctAnswer);
+        });
+        this.displayNext();
+        // console.log(this.state.selectedValues);
+    }
+
+    componentWillUnmount() {
+        this.correctValues.length = 0;
+    }; 
+
+    displayNext = () => {
+        if(this.state.currentPage < 1) {
+            this.setState({
+                currentPage: this.state.currentPage + 1,
+                start: this.state.end,
+                end: this.state.start + this.state.end + this.numberOfQuestionsPerPage,
+            })
+        }
+        else {
+            alert("EON");
+        }
+    }
+
+    displayPrevious= () => {
+        if(this.state.currentPage > 0) {
+            this.setState({
+                currentPage: this.state.currentPage - 1,
+                start: this.state.start - this.numberOfQuestionsPerPage,
+                end: this.state.start,
+            })
+        }
+        else {
+            alert("EOP")
+        }
+    }
+
+    handleSubmission = () => {
+        const tick = `&#x2714;`; const cross = `&#x2716;`;
+        const allForms = document.getElementsByClassName("optionGroup");
+
+        for(let i = 0 ; i < allForms.length; i++) {
+            const selected = allForms[i].querySelector(
+                "input[type='radio']:checked"
+            );
+            if (selected) {
+                const feedbackElement = document.createElement("span");
+                if(selected.value === this.correctValues[i]) {
+                    feedbackElement.className = "tick";
+                    feedbackElement.innerHTML = tick;
+                }
+                else {
+                    feedbackElement.className = "cross";
+                    feedbackElement.innerHTML = cross;
+                }
+                selected.parentNode.appendChild(feedbackElement);
+            } 
+        }
+
+        document.querySelectorAll('li.correct').forEach(answer => {
+            answer.style.color = "blue";
+            answer.style.fontWeight = "bold";
+        });
+    }
+
+    handleRadioChange = (event, question) => {
+        const selectedValue = event.target.id;
+        console.log(selectedValue);
+        const updateSelectedOptions = {
+            ...this.state.selectedOptions,
+            [question]: selectedValue
+        };
+        this.setState({
+            selectedOptions: updateSelectedOptions
+        });
+        localStorage.setItem(`myRadioValue-${question}`, selectedValue);
+    };
+
+    render() {
+
+        const questions = this.quizData.slice(this.state.start,
+            this.state.end).map((item, qindex) => {
+                const elem = {
+                    id: crypto.randomUUID(),
+                    question: item.question,
+                    options: item.options.map((choice, aindex) => {
+                        const choiceId = (this.state.currentPage).toString() + (qindex).toString() + aindex.toString();
+                        const correctAnswer = item.options[item.correctAnswerIndex];
+                        const isCorrect = (choice === correctAnswer);
+                        const className =  isCorrect ? "correct" : "";
+                        const qtag = "question" + (qindex + 1)
+                        return (
+                            <li key={choiceId} className={className}>
+                            <input 
+                                type="radio" 
+                                name={item.question} 
+                                id={choiceId}
+                                value={choice}
+                                checked={this.state.selectedOptions[qtag] === choiceId}
+                                onChange={ 
+                                    (event) => this.handleRadioChange(event, qtag) }
+                            />
+                            <label htmlFor={choiceId}>
+                            <span>{choice}</span></label>
+                            </li>
+                        )
+                    })
+                };
+
+                return (
+                    <React.Fragment key={elem.id}>
+                    <li className="questions">{elem.question}</li>
+                    <form className="optionGroup">
+                    <ol className="choices">{elem.options}</ol>
+                    </form>
+                    </React.Fragment>
+                );
+            })
+        return (
+            <>
+            <SEO title="NEC Exam 2079" name="Biomedical License"
+            description="Nepal Engineering Council Exam for Biomedical
+            Engineering 2079" type="article" />
+            <h1>Welcome to the Quiz!</h1>
+            <ol start={this.state.start + 1}>{questions}</ol>
+            <div id="buttons">
+                <button id="previous" type="submit" 
+                onClick={this.displayPrevious}>Previous</button>
+                <button id="next" type="submit" 
+                onClick={this.displayNext}>Next</button>
+                <button id="submit" type="submit" 
+                onClick={this.handleSubmission}>Submit</button>
+            </div>
+            </>
+        )
+    };
+}
+
+
+
+
+
+
+
 export class Semesters extends React.Component {
 
     constructor(props) {
@@ -73,146 +238,4 @@ export class Semesters extends React.Component {
             </>
         );
     }
-}
-
-export class Quiz extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.correctValues = [];
-        this.quizData = loadQuiz();
-        this.numberOfQuestionsPerPage = 5;
-        this.state = {
-            currentPage: -1,
-            start: 0,
-            end: 0
-        }
-    }
-
-    componentDidMount() {
-        this.quizData.forEach((item) => {
-            const correctAnswer = item.options[item.correctAnswerIndex];
-            this.correctValues.push(correctAnswer);
-        });
-        this.displayNext();
-
-    }
-
-    componentWillUnmount() {
-        this.correctValues.length = 0;
-    }; 
-
-    displayNext = () => {
-        if(this.state.currentPage < 1) {
-            this.setState({
-                currentPage: this.state.currentPage + 1,
-                start: this.state.end,
-                end: this.state.start + this.state.end + this.numberOfQuestionsPerPage,
-            })
-        }
-        else {
-            alert("EON");
-        }
-    }
-
-    displayPrevious= () => {
-        if(this.state.currentPage > 0) {
-            this.setState({
-                currentPage: this.state.currentPage - 1,
-                start: this.state.start - this.numberOfQuestionsPerPage,
-                end: this.state.start,
-            })
-        }
-        else {
-            alert("EOP")
-        }
-    }
-
-    submitAllForms = () => {
-        const tick = `&#x2714;`; const cross = `&#x2716;`;
-        const allForms = document.getElementsByClassName("optionGroup");
-
-        for(let i = 0 ; i < allForms.length; i++) {
-            const selected = allForms[i].querySelector(
-                "input[type='radio']:checked"
-            );
-            if (selected) {
-                if(selected.value === this.correctValues[i]) {
-                    selected.parentNode.innerHTML +=
-                        '<span class="tick">' + tick + '</span>';
-                }
-                else {
-                    selected.parentNode.innerHTML +=
-                        '<span class="cross">' + cross + '</span>';
-                }
-            } 
-        }
-
-        document.querySelectorAll('li.correct').forEach(answer => {
-            answer.style.color = "blue";
-            answer.style.fontWeight = "bold";
-        });
-
-    }
-
-    render() {
-
-        const questions = this.quizData.slice(this.state.start,
-            this.state.end).map((item) => {
-
-            const elem = {
-                id: crypto.randomUUID(),
-                question: item.question,
-                options: item.options.map((choice) => {
-                    const choiceId = crypto.randomUUID();
-                    const correctAnswer = item.options[item.correctAnswerIndex];
-                    const isCorrect = (choice === correctAnswer);
-                    const className =  isCorrect ? "correct" : "";
-                    return (
-                        <li key={choiceId} className={className}>
-                        <input 
-                            type="radio" 
-                            name={item.question} 
-                            id={choiceId}
-                            value={choice}
-                        />
-                        <label htmlFor={choiceId}><span>{choice}</span></label>
-                        </li>
-                    )
-                })
-            };
-
-            return (
-                <React.Fragment key={elem.id}>
-                <li className="questions">{elem.question}</li>
-                <form className="optionGroup">
-                <ol className="choices">{elem.options}</ol>
-                </form>
-                </React.Fragment>
-            );
-        })
-        return (
-            <>
-            <SEO title="NEC Exam 2079" name="Biomedical License"
-            description="Nepal Engineering Council Exam for Biomedical
-            Engineering 2079" type="article" />
-
-            <h1>Welcome to the Quiz!</h1>
-
-            <ol start={this.state.start + 1}>{questions}</ol>
-
-            <div id="buttons">
-            <button id="previous" type="submit" onClick={this.displayPrevious}>
-            Previous
-            </button>
-            <button id="next" type="submit" onClick={this.displayNext}>
-            Next
-            </button>
-            <button id="submit" type="submit" onClick={this.submitAllForms}>
-            Submit
-            </button>
-            </div>
-            </>
-        )
-    };
 }
