8edf8de4d325ff98513b22724b0406c962951a53
Nikin Baidar <nikinbaidarr@gmail.com>
2024-04-09 10:55:53 +0545

    scales all covers to be 600x350

--- src/dataLoader.js
+++ src/dataLoader.js
@@ -18,16 +18,18 @@ import DigitalLogic  from './data/images/digital_logic.jpg';
 import DigitalSignal from './data/images/digital_signal.jpg';
 import Drawing       from './data/images/drawing.png';
 import Economics     from './data/images/economics.avif';
-import Epp     from './data/images/epp.avif';
-import Fonepay       from './data/images/fonepay.jpg';
+import Epp           from './data/images/epp.avif';
+import FonepayQR     from './data/images/fonepay.jpg';
 import Tdi           from './data/images/tdi.jpg';
-import Nn            from './data/images/nn.avif';
+import Nn            from './data/images/NN.avif';
 import Eem           from './data/images/eem.png';
-import Implantable           from './data/images/implantable.avif';
+import Implantable   from './data/images/implantable.avif';
+import Informatics   from './data/images/informatics.avif';
 import MedElx        from './data/images/med_electronics.jpg';
 import MedImg1       from './data/images/imagign1.avif';
-import MedicalImagingII from './data/images/imagign2.avif';
-import MedicalIndustryManagement from './data/images/mim.png';
+import MedImg2       from './data/images/imagign2.avif';
+import Mip           from './data/images/image_processing.png';
+import MiM           from './data/images/mim.png';
 import Embedded      from './data/images/embedded.webp';
 import Electrical    from './data/images/electrical.jpg';
 import Entrapment    from './data/images/Entrapment.png';
@@ -35,7 +37,7 @@ import Escape        from './data/images/escape.webp';
 import Fluid         from './data/images/fluid.avif';
 import LowPassFilter from './data/images/Low_pass_filter.jpg';
 import Tux           from './data/images/c7b8113247fecd83bd9b5ed5bd3f34d5.jpg';
-import Maintenance         from './data/images/equipmentmaintenance.avif';
+import Maintenance   from './data/images/equipmentmaintenance.avif';
 import Math1         from './data/images/Math1.jpg';
 import Math2         from './data/images/math2.jpg';
 import Math3         from './data/images/math3.jpg';
@@ -45,6 +47,7 @@ import Micro         from './data/images/microprocessors.png';
 import Physics       from './data/images/relativity.jpg';
 import ProjectMgmt   from './data/images/project.avif';
 import Research      from './data/images/research.jpg';
+import Robotics      from './data/images/robotics.jpg';
 import Sociology     from './data/images/sociology.jpg';
 import Structural    from './data/images/structural.jpg';
 import Biomechanics  from './data/images/biomechanics.jpg';
@@ -55,17 +58,18 @@ import Numerical     from './data/images/numerical.jpg';
 import TEQues        from './data/images/modelquestion01B64.png';
 import ImagingQues   from './data/images/mri_modalities.png';
 import ECGQues       from './data/images/ecg_lead_attachments.png';
-import PertQues       from './data/images/pertQues.png';
-import WeAcceptFonePay from './data/images/weacceptfonepay.png';
+import PertQues      from './data/images/pertQues.png';
+import FonePay       from './data/images/weacceptfonepay.png';
 
 
 export const ads = {
     BuyMeACoffee,
     ComingSoon,
     Tux,
-    Fonepay,
-    WeAcceptFonePay,
+    Fonepay: FonepayQR,
+    WeAcceptFonePay: FonePay,
 }
+
 
 export const quizQuestionImages = {
     ActiveLPF: LowPassFilter,
@@ -75,6 +79,7 @@ export const quizQuestionImages = {
     ECGQues,
     PertQues,
 }
+
 
 export const covers = {
     Anatomy1,
@@ -95,6 +100,7 @@ export const covers = {
     Instrumentation1: BioMedInsI,
     Instrumentation2: BioMedInsII,
     Implantable,
+    Informatics,
     Escape,
     Fluid,
     Physics,
@@ -107,9 +113,10 @@ export const covers = {
     Mechanical,    
     MedElx,    
     MedImg1,    
-    MedicalImagingII,    
+    MedicalImagingII: MedImg2,    
     Micro,    
-    Mim: MedicalIndustryManagement,    
+    Mim: MiM,    
+    Mip,    
     Belectronics: BElectronics,
     Drawing,
     Electrical,
@@ -118,6 +125,7 @@ export const covers = {
     computer : Computer,
     Computer2 : Computer2,
     Research,
+    Robotics,
     Probability,
     Project: ProjectMgmt,
     Sociology,
